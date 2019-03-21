d55669929b709e6341e707db7697cc1e09fe4e0c
Denylson Melo <143313+denylsonmelo@users.noreply.github.com>
2019-03-21 16:07:02 -0300

    adicionando card e comecando a falar de forms e ngmodel

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -10,12 +10,27 @@ export class Tab1Page {
   modoAdicao = true;
 
   titulo = 'Listagem de Alunos';
+  alunoASerCadastrado = new Aluno('', 'matricula', '');
 
   alunos: Array<Aluno> = [
     { nome: 'iuri', matricula: '20172tinf002', modulo: 'modulo IV' },
     { nome: 'elias', matricula: '20172tinf005', modulo: 'modulo IV' },
     { nome: 'davi', matricula: '20161tinf013', modulo: 'modulo 8' }
   ];
+
+  pegarValorTeclado(event) {
+    console.log(event);
+
+  }
+
+  adicionar() {
+    console.log(this.alunoASerCadastrado);
+    this.alunos.push({
+      nome: '',
+      matricula: '',
+      modulo: ''
+    });
+  }
 
   trocouValorToggle() {
     this.modoAdicao
