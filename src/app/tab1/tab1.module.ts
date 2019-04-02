8695c6999ce183fc4c0f58e6130919a97c4b0787
Denylson Melo <143313+denylsonmelo@users.noreply.github.com>
2019-04-02 15:08:56 -0300

    comecando a falar de reactive forms

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -2,14 +2,14 @@ import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
-    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild([{ path: '', component: Tab1Page }])
   ],
   declarations: [Tab1Page]
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+import { AlertController } from '@ionic/angular';
+
 import { Aluno } from '../models/classes.model';
 
 @Component({
@@ -7,10 +11,17 @@ import { Aluno } from '../models/classes.model';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-  modoAdicao = false;
 
+  formularioAluno = new FormGroup({
+    matriculaAluno: new FormControl(''),
+    nomeAluno: new FormControl(''),
+    moduloAluno: new FormControl(''),
+  });
+
+  modoAdicao = true;
   titulo = 'Listagem de Alunos';
   alunoASerCadastrado = new Aluno('', '', '');
+  alerta;
 
   alunos: Array<Aluno> = [
     { nome: 'iuri', matricula: '20172tinf002', modulo: 'modulo IV' },
@@ -18,10 +29,33 @@ export class Tab1Page {
     { nome: 'davi', matricula: '20161tinf013', modulo: 'modulo 8' }
   ];
 
-  adicionar() {
+  constructor(public alertController: AlertController) {}
+
+  async exibirAlerta() {
+     this.alerta = await this.alertController.create({
+      message: 'Salvando ...',
+      backdropDismiss: false,
+      mode: 'ios',
+      translucent: true
+    });
+
+    await this.alerta.present();
+  }
+
+  async fecharAlerta() {
+    await this.alerta.dismiss();
+  }
+
+  async adicionar() {
+    await this.exibirAlerta();
+
+    // localStorage.setItem('alunoCadastradoAgora', this.alunoASerCadastrado.nome);
+
     this.alunos.push(this.alunoASerCadastrado);
     this.alunoASerCadastrado = new Aluno('', '', '');
     this.modoAdicao = false;
+
+    await this.fecharAlerta();
   }
 
   trocouValorToggle() {
