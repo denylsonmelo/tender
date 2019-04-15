59cd6d676232317b52c2e35e83257274400053ce
Denylson Melo <143313+denylsonmelo@users.noreply.github.com>
2019-04-15 15:09:29 -0300

    continuando personalizando tela de pessoas

--- src/app/models/classes.model.ts
+++ src/app/models/classes.model.ts
@@ -1,7 +1,14 @@
-export class Aluno {
+export class Pessoa {
   constructor(
     public nome: string,
-    public matricula: string,
-    public modulo: string
+    public dataNascimento: Date,
+    public sexo: string,
+    public descricao: string,
+    public localizacao: string,
+    public emprego: string,
+    public estudo: string,
+    public urlImagem: string,
+    public mostrarIdade: boolean = true,
+    public mostrarLocalizacao: boolean = true,
   ) {}
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,36 @@
 import { Component } from '@angular/core';
+import { Pessoa } from '../models/classes.model';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+  pessoas: Array<Pessoa> = [
+    {
+      nome: 'Steve Rogers',
+      dataNascimento: new Date(1918, 7, 4),
+      sexo: 'masculino',
+      descricao: 'melhor idade, já fui um picolé em gelo, um clássico',
+      localizacao: 'wakanda',
+      emprego: 'nomade',
+      estudo: 'tenho',
+      mostrarIdade: true,
+      mostrarLocalizacao: true,
+      urlImagem: './../../assets/images/capitao.jpeg'
+    },
+    {
+      nome: 'Natasha Romanova',
+      dataNascimento: new Date(1928, 7),
+      sexo: 'feminino',
+      descricao: 'ferrões, relacionamentos complicados, contorcionistas',
+      localizacao: 'wakanda',
+      emprego: 'KGB, Shield, Vingadores',
+      estudo: 'Balé (professora e bailarina), espiã, vingadora',
+      mostrarIdade: true,
+      mostrarLocalizacao: true,
+      urlImagem: './../../assets/images/scarlet.jpeg'
+    }
+  ];
+}
