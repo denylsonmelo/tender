14abdc3147b9f14745eaba16a8efc533ab96ea23
Denylson Melo <143313+denylsonmelo@users.noreply.github.com>
2019-04-30 14:30:20 -0300

    modificando pagina de perfil para mostrar conecao com firebase

--- src/app/perfil/perfil.page.ts
+++ src/app/perfil/perfil.page.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.page.html',
-  styleUrls: ['./perfil.page.scss'],
+  styleUrls: ['./perfil.page.scss']
 })
 export class PerfilPage implements OnInit {
+  perfis: Observable<any[]>;
 
-  constructor() { }
+  constructor(banco: AngularFirestore) {
+    this.perfis = banco.collection('/perfis').valueChanges();
+    console.log(this.perfis);
 
-  ngOnInit() {
   }
 
+  ngOnInit() {}
 }
