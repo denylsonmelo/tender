f37d5c104fc5797fc27305a7a442e16f9a2a7624
Denylson Melo <143313+denylsonmelo@users.noreply.github.com>
2019-05-21 15:09:07 -0300

    personalizando a tela de login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  styleUrls: ['./login.page.scss']
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  login() {
+    this.auth.logarComFacebook()
+      .then(sucesso => {
+        this.router.navigate(['/tabs/tab2']);
+      })
+      .catch(erro => {
+        console.log('errrro');
+      });
   }
 
+  constructor(public auth: AuthService, private router: Router) {}
+
+  ngOnInit() {}
 }
